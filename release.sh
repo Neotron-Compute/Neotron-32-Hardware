0666b14da8782c8dc89026231be2ca485bd5223e
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-12-21 18:29:11 +0000

    Fixed up release script to move/rename files into separate dir.

--- release.sh
+++ release.sh
@@ -2,22 +2,40 @@
 
 set -euo pipefail
 
+PROJECT=neotron-32
+
 if [ ! -v DOCKER_CMD ]; then
-	echo "Wibble"
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	DOCKER_CMD="docker run --rm -it -v ${DIR}:/kicad-project productize/kicad-automation-scripts"
-else
-	echo "Wobble"
 fi
 
-echo "Running ERC..."
-${DOCKER_CMD} python -m kicad-automation.eeschema.schematic run_erc /kicad-project/neotron-32.sch /kicad-project/build
-echo "Printing schematic..."
-${DOCKER_CMD} python -m kicad-automation.eeschema.schematic export /kicad-project/neotron-32.sch neotron-32-schematic.pdf
-echo "Running DRC..."
-${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.run_drc /kicad-project/neotron-32.kicad_pcb /kicad-project/build
-echo "Generating gebers..."
-${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.plot /kicad-project/neotron-32.kicad_pcb /kicad-project/build
-echo "Plotting PCB as PDF..."
-${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.plot -f pdf /kicad-project/neotron-32.kicad_pcb /kicad-project/build
+TEMP_PATH=$(mktemp -d "${DIR}/kicad-automation-XXXXXX")
 
+TEMP_PATH_IN_DOCKER=${TEMP_PATH/$DIR/\/kicad-project}
+
+echo "TEMP_PATH=$TEMP_PATH"
+echo "TEMP_PATH_IN_DOCKER=$TEMP_PATH_IN_DOCKER"
+
+mkdir "${DIR}/build" || (echo "Refusing to build because output dir exists" && exit 1)
+
+echo "Running ERC..."
+${DOCKER_CMD} python -m kicad-automation.eeschema.schematic run_erc /kicad-project/${PROJECT}.sch ${TEMP_PATH_IN_DOCKER}/erc
+cp ${TEMP_PATH}/erc/${PROJECT}.erc ${DIR}/build
+
+echo "Printing Schematic..."
+${DOCKER_CMD} python -m kicad-automation.eeschema.schematic export -f pdf /kicad-project/${PROJECT}.sch ${TEMP_PATH_IN_DOCKER}/schematic
+cp ${TEMP_PATH}/schematic/${PROJECT}.pdf ${DIR}/build/${PROJECT}-schematic.pdf
+
+echo "Running DRC..."
+${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.run_drc /kicad-project/${PROJECT}.kicad_pcb ${TEMP_PATH_IN_DOCKER}/drc
+cp ${TEMP_PATH}/drc/drc_result.rpt ${DIR}/build/${PROJECT}.drc
+
+echo "Generating Gebers..."
+${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.plot /kicad-project/${PROJECT}.kicad_pcb ${TEMP_PATH_IN_DOCKER}/gerbers
+cp ${TEMP_PATH}/gerbers/${PROJECT}_gerbers.zip ${DIR}/build/${PROJECT}-gerbers.zip
+
+echo "Plotting PCB as PDF..."
+${DOCKER_CMD} python -m kicad-automation.pcbnew_automation.plot -f pdf /kicad-project/${PROJECT}.kicad_pcb ${TEMP_PATH_IN_DOCKER}/pcb
+cp ${TEMP_PATH}/pcb/${PROJECT}.pdf ${DIR}/build/${PROJECT}-pcb.pdf
+
+echo "Now run: sudo rm -rf ${TEMP_PATH}"
